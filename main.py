792286735f5df9eb523c58e3a4b7a27f573ff438
Tonwilly <135318264+Tonwilly@users.noreply.github.com>
2025-09-25 13:22:11 +0000

    24 wednesday assignment

---
+++ main.py
@@ -0,0 +1,22 @@
+# for loop
+for i in range(5):
+    print(i)
+
+print("Hello world")
+# nested for loop
+for i in range(5,0,-1):
+    for y in range(i):
+        print(y, end=" ")
+    print()
+
+# input with int method
+marks = int(input("Enter Marks: "))
+if marks >= 0 and marks <= 39:
+    print("Grade A")
+elif marks >=40 and marks <=69:
+    print("Grade C")
+elif marks>=70 and marks<=79:
+    print("Grade B")
+elif marks>=80 and marks<=100:
+    print("Grade A")
+

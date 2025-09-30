c405e8e404f366d1c64cf1490ca59bfc6fc3af0b
Tonwilly <135318264+Tonwilly@users.noreply.github.com>
2025-09-30 08:24:09 +0300

    inputing marks of 8 units

--- main.py
+++ main.py
@@ -10,13 +10,15 @@ for i in range(5,0,-1):
     print()
 
 # input with int method
-marks = int(input("Enter Marks: "))
-if marks >= 0 and marks <= 39:
-    print("Grade A")
-elif marks >=40 and marks <=69:
-    print("Grade C")
-elif marks>=70 and marks<=79:
-    print("Grade B")
-elif marks>=80 and marks<=100:
-    print("Grade A")
+y=0
+for i in range(8):
+    marks = int(input(f"Enter Marks{y+1}: "))
+    if marks >= 0 and marks <= 39:
+        print("Grade A")
+    elif marks >=40 and marks <=69:
+        print("Grade C")
+    elif marks>=70 and marks<=79:
+        print("Grade B")
+    elif marks>=80 and marks<=100:
+        print("Grade A")
 
